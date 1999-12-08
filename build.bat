3f1048cd03cb52f9cfa1cf57724e4701a0797f14
Bill <unknown>
1999-12-08 09:39:37 +0000

    *** empty log message ***

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
-set TAG=CSPOTRUN_0_9
-set SRCZIP=CSpotRunSrc0p9.zip
+set TAG=CSPOTRUN_0_9_1
+set SRCZIP=CSpotRunSrc0p9p1.zip
+set VERSION=0.9.1
+
+#set TAG=CSPOTRUN_0_9
+#set SRCZIP=CSpotRunSrc0p9.zip
+#set VERSION=0.9
 
 #set TAG=CSPOTRUN_0_8_3
 #set SRCZIP=CSpotRunSrc0p8p3.zip
@@ -14,16 +19,16 @@ mkdir %TAG%
 mkdir %OUTPUTDIR%
 cd %TAG%
 
-REM *** PULL
+###  PULL
 cvs export -r %TAG% cspotrun
 
-REM *** MAKE SOURCE ZIPBALL
+### MAKE SOURCE ZIPBALL
 cd cspotrun
 pkzip -recurse -path=current -add %OUTPUTDIR%\%SRCZIP% src\* *.txt
 
 cd src
 
-REM *** COMPILE
-make zips
+### COMPILE
+make -e VERSION=%VERSION% zips
 
 move *.zip %OUTPUTDIR%
