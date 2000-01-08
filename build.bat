ea45a725c5ce74f14a9b46a4949039243a51bedd
Bill <unknown>
2000-01-08 23:38:36 +0000

    0.9.4

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
-set TAG=CSPOTRUN_0_9_3
-set SRCZIP=CSpotRunSrc0p9p3.zip
-set VERSION=0.9.3
+set TAG=CSPOTRUN_0_9_4
+set SRCZIP=CSpotRunSrc0p9p4.zip
+set VERSION=0.9.4
+
+#set TAG=CSPOTRUN_0_9_3
+#set SRCZIP=CSpotRunSrc0p9p3.zip
+#set VERSION=0.9.3
 
 #set TAG=CSPOTRUN_0_9_2
 #set SRCZIP=CSpotRunSrc0p9p2.zip
