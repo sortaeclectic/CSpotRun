16a1f31ed233750c1d9f9e4c55ffefa47dbdcfdc
Bill <unknown>
2000-01-17 08:44:45 +0000

    0.9.6

--- build.bat
+++ build.bat
@@ -1,34 +1,10 @@
-
 set V1=0
 set V2=9
-set V3=5
+set V3=6
 
 set TAG=CSPOTRUN_%V1%_%V2%_%V3%
 set SRCZIP=CSpotRunSrc%V1%p%V2%p%V3%.zip
 set VERSION=%V1%.%V2%.%V3%
-
-#set TAG=CSPOTRUN_0_9_4
-#set SRCZIP=CSpotRunSrc0p9p4.zip
-#set VERSION=0.9.4
-
-#set TAG=CSPOTRUN_0_9_3
-#set SRCZIP=CSpotRunSrc0p9p3.zip
-#set VERSION=0.9.3
-
-#set TAG=CSPOTRUN_0_9_2
-#set SRCZIP=CSpotRunSrc0p9p2.zip
-#set VERSION=0.9.2
-
-#set TAG=CSPOTRUN_0_9_1
-#set SRCZIP=CSpotRunSrc0p9p1.zip
-#set VERSION=0.9.1
-
-#set TAG=CSPOTRUN_0_9
-#set SRCZIP=CSpotRunSrc0p9.zip
-#set VERSION=0.9
-
-#set TAG=CSPOTRUN_0_8_3
-#set SRCZIP=CSpotRunSrc0p8p3.zip
 
 set TEMPDRIVE=d:
 set TEMPDIR=%TEMPDRIVE%\temp
