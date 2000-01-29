c9ccaa6435231602b3076a390003228bbcc14884
Bill <unknown>
2000-01-29 00:33:52 +0000

    0.9.7 revs

--- build.bat
+++ build.bat
@@ -1,13 +1,17 @@
+@echo off
+
+pushd .
+
 set V1=0
 set V2=9
-set V3=6
+set V3=7
 
 set TAG=CSPOTRUN_%V1%_%V2%_%V3%
 set SRCZIP=CSpotRunSrc%V1%p%V2%p%V3%.zip
 set VERSION=%V1%.%V2%.%V3%
 
-set TEMPDRIVE=d:
-set TEMPDIR=%TEMPDRIVE%\temp
+set TEMPDRIVE=u:
+set TEMPDIR=%TEMPDRIVE%\tmp
 set OUTPUTDIR=%TEMPDIR%\%TAG%\output
 
 %TEMPDRIVE%
@@ -16,16 +20,21 @@ mkdir %TAG%
 mkdir %OUTPUTDIR%
 cd %TAG%
 
-###  PULL
+rem ###  PULL
 cvs export -r %TAG% cspotrun
 
-### MAKE SOURCE ZIPBALL
+rem ### MAKE SOURCE ZIPBALL
 cd cspotrun
 pkzip -recurse -path=current -add %OUTPUTDIR%\%SRCZIP% src\* *.txt
-
+pkzip -recurse -path=current -add %OUTPUTDIR%\resources.zip src\res\*.rcp 
 cd src
 
-### COMPILE
+rem ### COMPILE
 make -e VERSION=%VERSION% zips
 
 move *.zip %OUTPUTDIR%
+
+popd
+
+@echo ### The crap should be in %OUTPUTDIR%
+
