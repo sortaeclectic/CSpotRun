98e09c828bcfec21fef82863bf4a824cf750737d
bill <unknown>
2001-07-11 21:26:41 +0000

    the screen wasnt redrawn when you hit the "goto" button on the bookmark
    the screen wasnt redrawn when you hit the "goto" button on the bookmark
    form.

--- src/bmkedfrm.c
+++ src/bmkedfrm.c
@@ -33,21 +33,21 @@
 
 #ifdef ENABLE_BMK
 
-static Boolean	_BmkEdFormHandleEvent(EventType *e);
+static Boolean  _BmkEdFormHandleEvent(EventType *e);
 
 
-static FormPtr	formPtr;
-static ListPtr	listPtr;
+static FormPtr  formPtr;
+static ListPtr  listPtr;
 
 Boolean BmkEdFormHandleEvent(EventType *e)
 {
-	Boolean        handled = false;
+    Boolean        handled = false;
 
-	CALLBACK_PROLOGUE
-	handled = _BmkEdFormHandleEvent(e);
-	CALLBACK_EPILOGUE
+    CALLBACK_PROLOGUE
+    handled = _BmkEdFormHandleEvent(e);
+    CALLBACK_EPILOGUE
 
-	return handled;
+    return handled;
 }
 
 
@@ -59,113 +59,114 @@ Boolean BmkEdFormHandleEvent(EventType *e)
  */
 static Boolean _BmkEdFormHandleEvent(EventType *e)
 {
-	EventType listRedrawEvt = {bmkRedrawListEvt, 0, 0, 0, {}};
-	Err err;
+    EventType listRedrawEvt = {bmkRedrawListEvt, 0, 0, 0, {}};
+    Err err;
 
-	/* sel is static because it needs to keep selection */
-	/* position after returning from the bmknamefrm */
-	static UInt16 sel = noListSelection;
+    /* sel is static because it needs to keep selection */
+    /* position after returning from the bmknamefrm */
+    static UInt16 sel = noListSelection;
 
-	switch(e->eType)
-	{
-	case ctlSelectEvent:
-		switch (e->data.ctlSelect.controlID)
-		{
-		case buttonID_cancel:
-			EvtAddEventToQueue(&listRedrawEvt);
-			FrmReturnToForm(0);
-			return true;
+    switch(e->eType)
+    {
+    case ctlSelectEvent:
+        switch (e->data.ctlSelect.controlID)
+        {
+        case buttonID_cancel:
+            EvtAddEventToQueue(&listRedrawEvt);
+            FrmReturnToForm(0);
+            return true;
 
-		case buttonID_bmkGoTo:
-			sel = LstGetSelection(listPtr);
-			if(sel == noListSelection)
-				return true;
+        case buttonID_bmkGoTo:
+            sel = LstGetSelection(listPtr);
+            if(sel == noListSelection)
+                return true;
 
-			BmkGoTo(sel, 0);
-			EvtAddEventToQueue(&listRedrawEvt);
-			FrmReturnToForm(0);
-			return true;
+            BmkGoTo(sel, 0);
+            EvtAddEventToQueue(&listRedrawEvt);
+            FrmReturnToForm(0);
+            FrmUpdateForm(formID_main, 0);
+            return true;
 
-		case buttonID_bmkDelete:
-			sel = LstGetSelection(listPtr);
-			if(sel == noListSelection)
-				return true;
+        case buttonID_bmkDelete:
+            sel = LstGetSelection(listPtr);
+            if(sel == noListSelection)
+                return true;
 
-			err = BmkDelete(sel);
+            err = BmkDelete(sel);
 
-			/* if current list selection is 0  */
-			/* then, since sel is unsigned,    */
-			/* substricting of 1 will wrap sel */
-			/* and it will hold 0xffff which is */
-			/* equal to 'noListSelection'      */
-			if(sel == LstGetNumberOfItems(listPtr) - 1)
-				sel--;
+            /* if current list selection is 0  */
+            /* then, since sel is unsigned,    */
+            /* substricting of 1 will wrap sel */
+            /* and it will hold 0xffff which is */
+            /* equal to 'noListSelection'      */
+            if(sel == LstGetNumberOfItems(listPtr) - 1)
+                sel--;
 
-			goto RET_REDRAW;
+            goto RET_REDRAW;
 
-		case buttonID_bmkRename:
-			sel = LstGetSelection(listPtr);
-			if(sel == noListSelection)
-				return true;
-		    	FrmPopupForm(formID_bmkName);
-			return true;
+        case buttonID_bmkRename:
+            sel = LstGetSelection(listPtr);
+            if(sel == noListSelection)
+                return true;
+                FrmPopupForm(formID_bmkName);
+            return true;
 
-		case buttonID_bmkMoveUp:
-			sel = LstGetSelection(listPtr);
-			if(sel == noListSelection)
-				return true;
+        case buttonID_bmkMoveUp:
+            sel = LstGetSelection(listPtr);
+            if(sel == noListSelection)
+                return true;
 
-			err = BmkMove(MOVE_UP, sel);
+            err = BmkMove(MOVE_UP, sel);
 
-			if(!err) sel = sel ? --sel : 0;
+            if(!err) sel = sel ? --sel : 0;
 
-			goto RET_REDRAW;
+            goto RET_REDRAW;
 
-		case buttonID_bmkMoveDown:
-			sel = LstGetSelection(listPtr);
-			if(sel == noListSelection)
-				return true;
+        case buttonID_bmkMoveDown:
+            sel = LstGetSelection(listPtr);
+            if(sel == noListSelection)
+                return true;
 
-			if(sel == LstGetNumberOfItems(listPtr) - 1)
-				return true;
+            if(sel == LstGetNumberOfItems(listPtr) - 1)
+                return true;
 
-			err = BmkMove(MOVE_DOWN, sel);
+            err = BmkMove(MOVE_DOWN, sel);
 
-			if(!err) sel++;
+            if(!err) sel++;
 
-			goto RET_REDRAW;
-		}
-		break;
-
-
-	case frmOpenEvent:
-		sel = noListSelection;
-		formPtr = FrmGetActiveForm();
-		listPtr = FrmGetObjectPtr(formPtr,
-			FrmGetObjectIndex(formPtr, listID_bmkEd));
-		BmkPopulateList(listPtr, 0, 0);
-		FrmDrawForm(formPtr);
-		return true;
+            goto RET_REDRAW;
+        }
+        break;
 
 
-	case bmkNameFrmOkEvt:
-		/* rename the bookmark, new name is in 'bmkName' */
-		err = BmkRename(sel, bmkName);
-		goto RET_REDRAW;
-	}
-	
-	return false;
+    case frmOpenEvent:
+        sel = noListSelection;
+        formPtr = FrmGetActiveForm();
+        listPtr = FrmGetObjectPtr(formPtr,
+            FrmGetObjectIndex(formPtr, listID_bmkEd));
+        BmkPopulateList(listPtr, 0, 0);
+        FrmDrawForm(formPtr);
+        return true;
+
+
+    case bmkNameFrmOkEvt:
+        /* rename the bookmark, new name is in 'bmkName' */
+        err = BmkRename(sel, bmkName);
+        goto RET_REDRAW;
+    }
+
+    return false;
 
 RET_REDRAW:
-	if(!err) {
-		err = BmkPopulateList(listPtr, 0, 0);
-		LstDrawList(listPtr);
-		LstSetSelection(listPtr, sel);
-	} else {
-		BmkReportError(err);
-	}
+    if(!err) {
+        err = BmkPopulateList(listPtr, 0, 0);
+        LstDrawList(listPtr);
+        LstSetSelection(listPtr, sel);
+    } else {
+        BmkReportError(err);
+    }
 
-	return true;
+    return true;
 }
 
 
